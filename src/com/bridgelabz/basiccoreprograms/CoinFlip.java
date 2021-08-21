5bbe32ce47145f4dff3677d09a1a51e70287c860
Aravindhan V <aravindhan.v@perfios.com>
2021-08-21 16:45:53 +0530

    ADDED: Implemented Coin Flipping Simulator

--- src/com/bridgelabz/basiccoreprograms/CoinFlip.java
+++ src/com/bridgelabz/basiccoreprograms/CoinFlip.java
@@ -1,5 +1,21 @@
 package com.bridgelabz.basiccoreprograms;
 
 public class CoinFlip {
+	public static void main(String[] args) {
+		int timesToFlip = 5;
+		int tailsCount = 0;
 
+		if (timesToFlip > 0) {
+			for (int i = 0; i < timesToFlip; i++) {
+				double randomNumber = Math.random();
+				if (randomNumber < 0.5) {
+					tailsCount++;
+				}
+			}
+			double tailsPercent = 100*tailsCount/timesToFlip;
+			System.out.println("Tails: "+tailsPercent+"%\nHeads: "+(100-tailsPercent)+"%");
+		} else {
+			System.err.println("Time to flip coin must be positive");
+		}
+	}
 }
