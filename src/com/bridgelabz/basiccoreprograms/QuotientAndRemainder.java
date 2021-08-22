a474c2f9f296a686a7226d4f85b3d8ec6cb8ef84
Aravindhan V <aravindhan.v@perfios.com>
2021-08-22 16:31:49 +0530

    ADDED: Calculator to display quotient and remainder

---
+++ src/com/bridgelabz/basiccoreprograms/QuotientAndRemainder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class QuotientAndRemainder {
+	public static void main(String[] args) {
+		int dividend = 11;
+		int divisor = 4;
+		System.out.println("Quotient = "+(dividend/divisor));
+		System.out.println("Remainder = "+(dividend%divisor));
+	}
+}
